d75aeaf3f450fb4cb7db2d5d224ca4f16a7b19e0
David Wetterau <david.wetterau@gmail.com>
2022-04-22 16:16:31 -0400

    freeze deps and actually activate the pump

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import os
 import airtable
 
+from gpiozero import Robot
+
+def pump():
+    # These numbers correspond to the pins we use on the Raspberry Pi.
+    # Only 7/8 are configured for our single-pump setup, but we have to specify both
+    r = Robot(left=(7, 8), right=(9, 10))
+    try:
+        r.backward(speed=1)
+        # Run for five seconds.
+        time.sleep(5)
+    finally:
+        r.stop()
+
+
 def main():
     base_id = os.getenv("BASE_ID")
     api_key = os.getenv("API_KEY")
