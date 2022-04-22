3c269cb30c85129d42ceda56284391ed987b8640
David Wetterau <david.wetterau@gmail.com>
2022-04-22 19:38:10 -0400

    rewrite to be http server

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
 import airtable
-import time
-
 from gpiozero import Robot
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import os
+import time
 
 def pump():
     # These numbers correspond to the pins we use on the Raspberry Pi.
@@ -16,7 +17,7 @@ def pump():
         r.stop()
 
 
-def main():
+def check_airtable():
     base_id = os.getenv("BASE_ID")
     api_key = os.getenv("API_KEY")
     table_name = os.getenv("TABLE_ID")
@@ -33,6 +34,42 @@ def main():
         pump()
         print("Done!")
         at.update(table_name, pump_record["id"], {"State": "Done"})
+
+form_page = b"""
+<html><form action=\"/\" method=\"POST\"><input type=\"submit\" value=\"Pump\"></form></body></html>
+"""
+
+class Handler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request")
+        self._set_response()
+        self.wfile.write(form_page)
+
+    def do_POST(self):
+        logging.info("POST request")
+        try:
+            pump()
+        except Exception:
+            pass
+        self._set_response()
+        self.wfile.write(form_page)
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, Handler)
+    logging.info('Starting server...')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Server stopped.')
 
 if __name__ == "__main__":
     main()
